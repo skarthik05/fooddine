f71fbc9a0e3222c3a3c84f8db46ff78bb2531b26
Karthiks0598 <karthik.s@verzeo.com>
2022-06-11 20:29:22 +0530

    chore: added recent orders, listingactive orders, signin and signout

--- server/controller/order.js
+++ server/controller/order.js
@@ -1,7 +1,92 @@
-const mongodb = require("../mongodb");
-exports.createOrder = (req, res) => {};
+const { dateToISo } = require("../handler/function");
+const mongoDb = require("../mongodb");
+exports.createOrders = async (req, res) => {
+  let { phone = null, tableNumber, items } = req.body;
+  for (let item of items) {
+    let query = {
+      $and: [
+        {
+          phone: phone,
+          tableNumber: tableNumber,
+          isCompleted: false,
+          item: item.item,
+        },
+      ],
+    };
+    await mongoDb
+      .get("menus")
+      .updateOne({ item: item.item }, { $inc: { count: item.quantity } });
+    let orderCheckOrderDetails = await mongoDb.get("orders").findOne(query);
+    if (orderCheckOrderDetails) {
+      mongoDb
+        .get("orders")
+        .findOne(query, { $inc: { quantity: item.quantity } });
+    } else {
+      item["createdOn"] = dateToISo();
+      item["phone"] = phone;
+      item["isCompleted"] = false;
+      mongoDb.get("orders").insertOne(item);
+    }
+  }
+};
 
-exports.listOrder = async (req, res) => {
-  let orderList = await mongodb.get("orders").find().toArray();
-  console.log({ orderList });
+exports.listCurrentOrders = async (req, res) => {
+  let pipeline = [
+    {
+      $match: {
+        $and: [{ date: dateToISo() }, { isCompleted: false }],
+      },
+    },
+    {
+      $group: {
+        _id: { tableNumber: "$tableNumber" },
+        tables: {
+          $addToSet: {
+            item: "$item",
+            isCompleted: "$isCompleted",
+            quantity: "$quantity",
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        tableNumber: "$_id.tableNumber",
+        _id: 0,
+        tables: 1,
+      },
+    },
+  ];
+  let orderList = await mongoDb
+    .get("orders")
+    .aggregate(pipeline)
+    .toArray()
+    .toArray();
+  if (!orderList?.length) {
+    return res.status(200).send([]);
+  }
+  return res.status(200).send(orderList);
+};
+
+exports.filterOrders = async (req, res) => {
+  let { date = dateToISo() } = req.body;
+  let orderDetails = await mongoDb.get("orders").find({ date: date }).toArray();
+  if (!orderDetails?.length) {
+    return res.status(200).send([]);
+  }
+  return res.status(200).send(orderDetails);
+};
+
+exports.listOrders = async (req, res) => {
+  let orderDetails = await mongoDb
+    .get("menus")
+    .find({})
+    .project({ _id: 0, item: 1, price: 1 })
+    .sort({ count: -1 })
+    .limit(6)
+    .toArray();
+  if (!orderDetails?.length) {
+    return res.status(200).send([]);
+  }
+  return res.status(200).send(orderDetails);
 };
--- server/controller/user.js
+++ server/controller/user.js
@@ -1,2 +1,82 @@
+const mongoDb = require("../mongodb");
+const jwt = require("jsonwebtoken");
+const { dateToISo, nanoId } = require("../handler/function");
 //user auth
-exports.signUp = (req, res) => {};
+exports.signUp = async (req, res) => {
+  let {
+    name = null,
+    email = null,
+    phone = null,
+    tableNumber,
+    items,
+  } = req.body;
+  if (name && email && phone) {
+    let token = jwt.sign(req.body, process.env.ACCESSTOKEN);
+    res.status(201).json({ token, items });
+    // // dateToISo
+    items.forEach((item) => {
+      mongoDb
+        .get("menus")
+        .updateOne({ item: item.item }, { $inc: { count: item.quantity } });
+      item["createdOn"] = dateToISo();
+      item["phone"] = phone;
+      item["isCompleted"] = false;
+    });
+    let insertDetails = Promise.all([
+      mongoDb
+        .get("users")
+        .updateOne(
+          { $or: [{ email: email }, { phone, phone }] },
+          { $set: req.body },
+          { upsert: true }
+        ),
+      mongoDb.get("orders").insertMany(items),
+      mongoDb
+        .get("tables")
+        .updateOne(
+          { tableNumber: tableNumber },
+          { $set: { isOccupied: true } }
+        ),
+    ]);
+  }
+  return res.status(409).send("All fields are required");
+};
+
+exports.signOut = async (req, res) => {
+  let { tableNumber, phone } = req.body;
+  let updateDetails = await Promise.all([
+    mongoDb.get("orders").updateMany(
+      {
+        $and: [
+          { phone: phone },
+          { tableNumber: tableNumber },
+          { isCompleted: false },
+        ],
+      },
+      { $set: { isCompleted: true } }
+    ),
+    mongoDb
+      .get("tables")
+      .updateOne({ tableNumber: tableNumber }, { $set: { isOccupied: false } }),
+  ]);
+  return res.sendStatus(200);
+};
+
+exports.recentOrders = async (req, res) => {
+  let { email, phone } = req.body;
+  if (email && phone) {
+    let orderDetails = await mongoDb
+      .get("orders")
+      .find({ $or: [{ email }, { phone }] })
+      .project({ _id: 0, item: 1 })
+      .sort({ date: -1, quantity: -1 })
+      .limit(6)
+      .toArray();
+
+    if (!orderDetails?.length) {
+      return res.status(200).send([]);
+    }
+    return res.status(200).send(orderDetails);
+  }
+  return res.status(409).send("User details required to show recent orders");
+};
