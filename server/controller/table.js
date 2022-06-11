cf35855a856125d80db689cb25d939c9e47defda
Karthiks0598 <karthik.s@verzeo.com>
2022-06-11 22:53:48 +0530

    fix : order creation response after placing order

--- server/controller/table.js
+++ server/controller/table.js
@@ -1,15 +1,19 @@
 const mongoDb = require("../mongodb");
 
 exports.tableStatus = async (req, res) => {
-  let tableStats = await mongoDb
-    .get("tables")
-    .find()
-    .project({ _id: 0 })
-    .toArray();
-  if (!tableStats?.length) {
-    return res.status(200).send([]);
+  try {
+    let tableStats = await mongoDb
+      .get("tables")
+      .find()
+      .project({ _id: 0 })
+      .toArray();
+    if (!tableStats?.length) {
+      return res.status(200).send([]);
+    }
+    return res.status(200).send(tableStats);
+  } catch (error) {
+    return res.sendStatus(500);
   }
-  return res.status(200).send(tableStats);
 };
 
 exports.tableInformation = (req, res) => {};
